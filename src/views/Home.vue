00287da01840d69b4d3eb6eab59a1273554ba82d
奥田将輝 <shoki0919kurosho@outlook.jp>
2021-03-13 11:25:30 +0900

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <input type="text" v-model="text">
+    <button @click="userClick">住所自動入力</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      text:""
+    };
+  },
+  methods:{
+    userClick(){
+      this.text.push(
+        
+      )
+    }
+  }
+}
+</script>
